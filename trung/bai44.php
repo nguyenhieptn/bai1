023687b0ef53ffa85b2fe60b77da2d80750921ef
trungah <mrtrung.nl@gmail.com>
2015-01-25 21:48:54 +0700

    trung
    up

---
+++ trung/bai44.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+	<html>
+	<head>
+		<title>bai 4</title>
+	</head>
+	<body>
+		<form action="#" method="get">
+			<label for="name">Nhập N :</label>
+			<input type="text" name="n" >
+			<input type="submit"  value="hien thi">
+		</form>
+		<?php 
+		
+			$A=1;
+			$A++;
+			echo '$A';
+		?>
+	</body>
+	
+	</html>	
